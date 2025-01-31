fffc9770e6c1b3026e81554260fe813319702003
gianlucafarias <72280809+gianlucafarias@users.noreply.github.com>
2025-01-30 21:58:36 -0300

    add feature refesh token

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,5 +1,5 @@
 import { Router, RequestHandler } from 'express';
-import { login, register, resetPassword } from '../controllers/auth.controller';
+import { login, register, resetPassword, refreshToken } from '../controllers/auth.controller';
 
 const router = Router();
 
@@ -7,6 +7,7 @@ const router = Router();
 router.post('/login', login as RequestHandler);
 router.post('/register', register as RequestHandler);
 router.post('/reset-password', resetPassword as RequestHandler);
+router.post('/refresh-token', refreshToken as RequestHandler);
 
 
 export default router;
