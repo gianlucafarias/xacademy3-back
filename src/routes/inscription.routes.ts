d5f042a3687ccdbd98f698556750f3a1916f60b4
M-VictoriaCM <mvittoria7@gmail.com>
2025-03-05 13:20:25 -0300

    Middleware para verificar el token del usuario

--- src/routes/inscription.routes.ts
+++ src/routes/inscription.routes.ts
@@ -1,9 +1,10 @@
 import { RequestHandler, Router } from "express";
 import { enrollStudentInCourse, getAllInscriptions, getInscriptionsByCourseId, getInscriptionsByStudentId } from "../controllers/inscription.controller";
+import verifyToken  from "../middleware/authMiddleware";
 
 const router = Router();
 router.get('/', getAllInscriptions as RequestHandler);
-router.post("/enroll", enrollStudentInCourse as RequestHandler);
+router.post("/enroll",  verifyToken, enrollStudentInCourse as RequestHandler);
 router.get('/:student_id', getInscriptionsByStudentId as RequestHandler)
 router.get('/courses/:course_id', getInscriptionsByCourseId as RequestHandler);
 export default router;
