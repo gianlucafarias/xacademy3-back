3b74cbe11e9ae373752edcddd53034372e3afca1
M-VictoriaCM <mvittoria7@gmail.com>
2025-03-09 14:07:12 -0300

    modificaciones en el modelo

--- src/models/Assist.ts
+++ src/models/Assist.ts
@@ -1,10 +1,10 @@
-const { DataTypes } = require("sequelize");
-const sequelize = require("config");
+import { DataTypes } from "sequelize";
+import { sequelize } from "../../config/dbConfig";
 import Courses from "./Courses";
 import Student from "./Student";
 
 const Assist = sequelize.define(
-  "Assist",
+  "Assists",
   {
     id: {
       type: DataTypes.INTEGER,
@@ -21,22 +21,48 @@ const Assist = sequelize.define(
       onDelete: "CASCADE",
     },
     student_id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        references: {
-          model: Student,
-          key: "id",
-        },
-        onDelete: "CASCADE",
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: Student,
+        key: "id",
+      },
+      onDelete: "CASCADE",
     },
-    attendanceDate:{
-        type: DataTypes.DATE,
-        allowNull: false,
+    attendanceDate: {
+      type: DataTypes.DATE,
+      allowNull: false,
     },
+    attendance: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: false
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+    updatedAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    }
   },
   {
-    tableName: "assist",
+    tableName: "assists",
+    timestamps: true,
   }
 );
+
+Assist.belongsTo(Courses, {
+  foreignKey: "course_id",
+  as: "course", // Cambio de "courses" a "course"
+});
+
+Assist.belongsTo(Student, {
+  foreignKey: "student_id",
+  as: "student", // Correcto
+});
 
 export default Assist;
--- src/routes/assist.routes.ts
+++ src/routes/assist.routes.ts
@@ -1,5 +1,10 @@
 import { RequestHandler, Router } from 'express';
+import { getAllAssistByIdCurse } from '../controllers/assist.controller';
 
 const router = Router();
+
+router.get('/assitance/:course_id', getAllAssistByIdCurse as RequestHandler);
+
+
 
 export default router;
