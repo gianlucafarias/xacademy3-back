f14cb2b51a80ce2f3afc3a4260281b3ae0d1dfcd
M-VictoriaCM <mvittoria7@gmail.com>
2025-03-14 10:53:36 -0300

    creacion de la logica para poder calificar un estudiante

--- src/routes/student.routes.ts
+++ src/routes/student.routes.ts
@@ -1,9 +1,11 @@
 import { RequestHandler, Router } from "express";
-import { getAllStudents, getStudentById } from "../controllers/student.controller";
+import {  assignFinalGrade, getAllStudents, getStudentById, updateStudentGrade } from "../controllers/student.controller";
 
 const router = Router();
 
 router.get('/', getAllStudents as RequestHandler);
 router.get('/view/:id', getStudentById as RequestHandler);
+router.post('/qualify/:id', assignFinalGrade  as RequestHandler);
+router.put('/update-grade/:id', updateStudentGrade as RequestHandler);
 
 export default router;
