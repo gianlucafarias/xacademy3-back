ead12bc13d1fcaf9dafdbc9d632ab425b5016c37
M-VictoriaCM <mvittoria7@gmail.com>
2025-03-15 11:46:38 -0300

    enpoint para mostrar el porcentaje de asistencia de un alumno

--- src/controllers/student.controller.ts
+++ src/controllers/student.controller.ts
@@ -2,8 +2,8 @@ import { Request, Response } from "express";
 import Student from "../models/Student";
 import Courses from "../models/Courses";
 import User from "../models/User";
-import { error } from "console";
-import { QueryInterface, where } from "sequelize";
+import Class from "../models/Class";
+import Assist from "../models/Assist";
 
 //Mostrar todos los estudiantes
 export const getAllStudents = async (req: Request, res: Response) => {
@@ -18,11 +18,16 @@ export const getAllStudents = async (req: Request, res: Response) => {
   }
 }
 
+//Verifico si existe el estudiante
+const findStudentById = async (id: string) => {
+  return await Student.findByPk(id);
+};
+
 //Mostrar estudiantes por id
 export const getStudentById = async (req: Request, res: Response) => {
   const { id } = req.params;
   try {
-    const student = await Student.findByPk(id);
+    const student = findStudentById(id);
     if (!student) {
       return res.status(404).json({ error: 'Curso no encontrado' });
     }
@@ -78,10 +83,7 @@ export const assignFinalGrade = async (req: Request, res: Response): Promise<voi
     res.status(500).json({ message: "Error al registrar la calificación" });
   }
 };
-//Verifico si existe el estudiante
-const findStudentById = async (id: string) => {
-  return await Student.findByPk(id);
-};
+
 
 // valido la calificacion
 const validateQualification = (qualification: any): boolean => {
@@ -160,3 +162,62 @@ export const getConditionByStudentId = async (req: Request, res: Response) => {
   }
   
 }
+
+/**
+ * Obtener el porcentaje de asistencias de un estudiante
+ */
+
+export const getAttendancePercentage =async(req:Request, res:Response)=>{
+  try {
+    const {id} =req.params;
+    const studentId = parseInt(id, 10);
+    
+    if (isNaN(studentId)) {
+      return res.status(400).json({ message: "ID de estudiante inválido" });
+    }
+
+    //verifico el estudiante
+    const student = findStudentById(id);
+    if(!student){
+      return res.status(404).json({
+        message:"El estudiante no encontrado"
+      });
+    }
+    //contar el total de las clases que el estudiante asistio
+    const totalClases = await Assist.count({
+      where:{student_id:id}
+    });
+
+    if(totalClases === 0){
+      return res.status(200).json({
+        id, percentage:0,
+        message:"El estudiante no tiene asistencia registrada"
+      });
+    }
+    //cuento la cantidad de clases que el estudiante asistio
+    const attendedClases= await Assist.count({
+      where:{student_id: studentId, attendance: 1}
+    });
+    // Depuración
+    console.log("Total Clases:", totalClases);
+    console.log("Clases Asistidas:", attendedClases);
+    //calculo el porcentaje
+
+  const attendancePercentage = (attendedClases / totalClases)*100;
+
+  res.status(200).json({
+    id, 
+    percentage: attendancePercentage.toFixed(2) + "%",
+    attended:attendedClases,
+    total: totalClases
+  });
+
+
+  } catch (error) {
+    console.error("El error al obtener el porcentaje de asistencia", error);
+    res.status(500).json({
+      error:"Error al obtener los porcentajes"
+    });
+  }
+}
+
