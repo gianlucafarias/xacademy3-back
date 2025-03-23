4254b6ab9d228bb277e722cca525c21515e01137
Sofjamanca <149734691+Sofjamanca@users.noreply.github.com>
2025-03-23 18:15:15 -0300

    Recuperando cambios del stash

--- src/routes/inscription.routes.ts
+++ src/routes/inscription.routes.ts
@@ -1,6 +1,7 @@
 import { RequestHandler, Router } from "express";
 import { updateUserData, enrollStudentInCourse, getAllInscriptions, getInscriptionsByCourseId,
-     getInscriptionsByStudentId, getStudentByUserId} from "../controllers/inscription.controller";
+     getInscriptionsByStudentId, getStudentByUserId,
+     getCountInscriptions} from "../controllers/inscription.controller";
 import verifyToken  from "../middleware/authMiddleware";
 
 const router = Router();
@@ -10,5 +11,6 @@ router.get('/view', verifyToken, getInscriptionsByStudentId as RequestHandler)
 router.put('/update', verifyToken, updateUserData as RequestHandler);
 router.get('/courses/:course_id', getInscriptionsByCourseId as RequestHandler);
 router.get('/student/:user_id', getStudentByUserId as RequestHandler);
+router.get('/countInscriptions', verifyToken, getCountInscriptions as RequestHandler);
 
 export default router;
