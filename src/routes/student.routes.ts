8983f38d99e90d1d1b059df08f8c337fb87de0d2
Sofjamanca <149734691+Sofjamanca@users.noreply.github.com>
2025-03-23 21:59:20 -0300

    cambios en cálculo de asistencias

--- src/routes/student.routes.ts
+++ src/routes/student.routes.ts
@@ -1,6 +1,6 @@
 import { RequestHandler, Router } from "express";
-import {  assignFinalGrade, getAllStudents, getAttendancePercentage, getConditionByStudentId, getStudentById, updateStudentGrade } from "../controllers/student.controller";
-
+import {  assignFinalGrade, getAllStudents, getAttendancePercentageByCourse, getConditionByStudentId, getStudentById, updateStudentGrade } from "../controllers/student.controller";
+import verifyToken  from "../middleware/authMiddleware";
 const router = Router();
 
 router.get('/', getAllStudents as RequestHandler);
@@ -8,5 +8,5 @@ router.get('/view/:id', getStudentById as RequestHandler);
 router.post('/qualify/:id', assignFinalGrade  as RequestHandler);
 router.put('/update-grade/:id', updateStudentGrade as RequestHandler);
 router.get('/condition/:id', getConditionByStudentId as RequestHandler);
-router.get('/:id/attendance', getAttendancePercentage as RequestHandler)
+router.get('/attendance/:studentId/:courseId', getAttendancePercentageByCourse as RequestHandler)
 export default router;
