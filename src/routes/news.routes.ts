9904271b00f417855ace739dd5776ee1f212e4b6
gianlucafarias <palmiergianluca@gmail.com>
2025-03-26 10:24:57 -0300

    add ordenamiento desde el back y seed para db

--- src/routes/news.routes.ts
+++ src/routes/news.routes.ts
@@ -1,5 +1,5 @@
 import { RequestHandler, Router } from 'express';
-import { createNews, getAllNews, getNewsById, updateNews, deleteNews } from '../controllers/news.controller';
+import { createNews, getAllNews, getNewsById, updateNews, deleteNews, getOrderedNews } from '../controllers/news.controller';
 
 
 
@@ -7,9 +7,10 @@ const router = Router();
 
 router.post('/', createNews as RequestHandler);
 router.get('/', getAllNews as RequestHandler);
-router.get('/:id', getNewsById as RequestHandler);
+router.get('/view/:id', getNewsById as RequestHandler);
 router.put('/:id', updateNews as RequestHandler);
 router.delete('/:id', deleteNews as RequestHandler);
+router.get('/ordered', getOrderedNews as RequestHandler);
 
 export default router;
 
